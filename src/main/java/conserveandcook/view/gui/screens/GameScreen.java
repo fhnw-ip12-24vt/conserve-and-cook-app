96d0a1f14c2e213f9d31c18e91a6b282dcabf7e0
Alessio <alessio.saccullo@students.fhnw.ch>
2024-12-04 19:48:30 +0100

    Erste Rohform des des Programmes

--- src/main/java/conserveandcook/view/gui/screens/GameScreen.java
+++ src/main/java/conserveandcook/view/gui/screens/GameScreen.java
@@ -1,7 +1,12 @@
 package conserveandcook.view.gui.screens;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GameScreen implements Screen {
     private int currentFrame = 0;
+    private Timer gametimer;
+    private int timeremaining = 120; //Spielzeit in Sekunden
 
     @Override
     public String getTitle() {
@@ -13,4 +18,33 @@ public class GameScreen implements Screen {
         String frameName = String.format("%01d", currentFrame);
         return backgroundPath + "/frame_" + frameName + ".png";
     }
+
+    public void startGameTimer() {
+        gametimer = new Timer();
+        gametimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                timeremaining--;
+                updateTimerDisplay();
+                if (timeremaining <= 0) {
+                    gametimer.cancel();
+                    endGame();
+                }
+            }
+        }
+    }
+
+    private void updateTimerDisplay() { //Anzeige von 60 Sekunden in 1 Minuten
+        int minutes = timeremaining / 60;
+        int seconds = timeremaining % 60;
+        String formatierteZeit = ; // Zeit in String formatieren
+        System.out.println(formatierteZeit); //Hier könnte die Anbindung ins GUI gemacht werden
+    }
+
+    private void endGame() {
+        System.out.println("GAME OVER");
+    }
 }
+
+
+
