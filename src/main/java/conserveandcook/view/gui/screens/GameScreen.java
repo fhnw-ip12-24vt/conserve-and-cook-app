c5876f366df15e4c000eab4b52cbebc12faa6d22
Simon <simon.kappeler@students.fhnw.ch>
2025-01-13 10:20:46 +0100

    GameScreen auf neue Architektur angepasst

--- src/main/java/conserveandcook/view/gui/screens/GameScreen.java
+++ src/main/java/conserveandcook/view/gui/screens/GameScreen.java
@@ -4,12 +4,9 @@ import ch.trick17.gui.Gui;
 import conserveandcook.model.Application;
 import conserveandcook.model.Ingredient;
 
-public class GameScreen implements Screen {
-    private int currentFrame = 0;
-    private int gameDuration = 120; //2 Minuten
-    private long lastUpdateTime;
-
 public class GameScreen extends AbstractScreen {
+    private int gameDuration = 120; // 2 minutes in seconds
+    private long lastUpdateTime;
 
     public GameScreen(Gui g) {
         super(g);
@@ -24,7 +21,7 @@ public class GameScreen extends AbstractScreen {
                 drawBorder(ingredient.getCategory());
             }
         }
-
+        gameTimer();
     }
 
     private void drawBorder(int category) {
@@ -32,16 +29,16 @@ public class GameScreen extends AbstractScreen {
         double x, y;
         switch (category) {
             case 0:
-                x = (double) 690 ;
-                y = (double) 175 ;
+                x = (double) 690;
+                y = (double) 175;
                 break;
             case 1:
-                x = (double) 930 ;
-                y = (double) 535 ;
+                x = (double) 930;
+                y = (double) 535;
                 break;
             case 2:
                 x = (double) 1210;
-                y = (double) 190 ;
+                y = (double) 190;
                 break;
             default:
                 return;
@@ -49,23 +46,19 @@ public class GameScreen extends AbstractScreen {
         gui.drawImage(borderPath, x * SCALE, y * SCALE, SCALE);
     }
 
-    public void startGame() {
-        lastUpdateTime = System.nanoTime();
-    }
-
     public void gameTimer() {
         long currentTime = System.nanoTime();
         long goneTime = currentTime - lastUpdateTime;
 
+        if (gameDuration <= 0) {
+            gameOver();
+            return;
+        }
         if (goneTime >= 1000000000L) {
             gameDuration--;
-            currentFrame = (currentFrame + 1) % 10;
             lastUpdateTime = currentTime;
         }
 
-        if (gameDuration <= 0) {
-            GameOver();
-        }
 
         drawTime();
     }
@@ -74,11 +67,11 @@ public class GameScreen extends AbstractScreen {
         int minutes = gameDuration / 60;
         int seconds = gameDuration % 60;
         String time = String.format("%02d:%02d", minutes, seconds);
-        // 50, 50 habe ich mal als Platzhalter für die Position
-        System.out.println("Draw String: " + time + " at (50, 50)");
+
+        gui.drawString(time, 50, 50);
     }
 
-    private void GameOver() {
-        System.out.println("GAME OVER");
+    private void gameOver() {
+        gui.drawString("Game Over", 50, 50);
     }
 }
