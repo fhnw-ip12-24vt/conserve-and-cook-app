1bb24d32a7573489577c7bea9e8534f52fd48eab
Simon <simon.kappeler@students.fhnw.ch>
2025-01-16 11:44:31 +0100

    Sprachen zu Enum konvertiert

--- src/main/java/conserveandcook/view/gui/screens/LanguageScreen.java
+++ src/main/java/conserveandcook/view/gui/screens/LanguageScreen.java
@@ -3,6 +3,8 @@ package conserveandcook.view.gui.screens;
 import ch.trick17.gui.Gui;
 import conserveandcook.model.Application;
 
+import java.util.Locale;
+
 public class LanguageScreen extends AbstractScreen {
     static final int FRAMES = 4;
     static final long GIF_INTERVAL = 2200 * 100000; // 0.25 seconds
@@ -13,7 +15,7 @@ public class LanguageScreen extends AbstractScreen {
 
     @Override
     public void draw(Application model) {
-        String lang = model.getSelectedLanguage();
+        String lang = model.getSelectedLanguage().name().toLowerCase();
         String gifNumber = getGifNumber();
         String currentFrame = "img/language/" + lang + gifNumber + ".png";
         drawBackground(currentFrame);
