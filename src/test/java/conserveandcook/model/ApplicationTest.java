4e5eceed3fc0b7dc1c62fb6bfc1b1d92f7299df6
Simon <simon.kappeler@students.fhnw.ch>
2025-01-16 11:44:59 +0100

    Unittests geschrieben

--- src/test/java/conserveandcook/model/ApplicationTest.java
+++ src/test/java/conserveandcook/model/ApplicationTest.java
@@ -3,8 +3,10 @@ package conserveandcook.model;
 import conserveandcook.AbstractTest;
 import conserveandcook.view.gui.AvailableScreens;
 import org.junit.jupiter.api.Test;
+import conserveandcook.misc.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest extends AbstractTest {
     @Test
@@ -41,17 +43,17 @@ public class ApplicationTest extends AbstractTest {
     @Test
     public void testLanguageSelect() {
         Application model = new Application();
-        String lang = model.getSelectedLanguage();
-        assertEquals("de", lang);
+        Languages lang = model.getSelectedLanguage();
+        assertEquals(Languages.DE, lang);
     }
 
     @Test
     public void testIncLanguageSelect() {
         Application model = new Application();
-        String[] langs = Application.getLanguages();
-        String lang;
+        Languages[] langs = Application.getLanguages();
+        Languages lang;
         lang = model.getSelectedLanguage();
-        assertEquals("de", lang);
+        assertEquals(Languages.DE, lang);
         for (int i = 0; i < langs.length - 1; i++) {
             model.incrementLanguage();
             lang = model.getSelectedLanguage();
@@ -60,20 +62,115 @@ public class ApplicationTest extends AbstractTest {
         // Looping
         model.incrementLanguage();
         lang = model.getSelectedLanguage();
-        assertEquals("de", lang);
+        assertEquals(Languages.DE, lang);
     }
 
     @Test
     public void getLanguages() {
-        String[] langs = {"de", "fr", "it", "en"};
-        String[] appLangs = Application.getLanguages();
+        String[] langs = {"DE", "FR", "IT", "EN"};
+        Languages[] appLangs = Application.getLanguages();
         for (int i = 0; i < langs.length; i++) {
-            assertEquals(langs[i], appLangs[i]);
+            assertEquals(langs[i], appLangs[i].name());
         }
     }
 
     @Test
-    public void testInc(){
+    public void testNextRegion() {
+        Application model = new Application();
+
+        Regions region = model.getSelectedRegion();
+        assertEquals(Regions.AMERICA, region);
+
+        model.nextRegion();
+        region = model.getSelectedRegion();
+        assertEquals(Regions.ASIA, region);
+
+        model.nextRegion();
+        region = model.getSelectedRegion();
+        assertEquals(Regions.EUROPE, region);
+
+        model.nextRegion();
+        region = model.getSelectedRegion();
+        assertEquals(Regions.AMERICA, region);
+    }
+
+    @Test
+    public void testPreviousRegion() {
+        Application model = new Application();
+
+        model.previousRegion();
+        Regions region = model.getSelectedRegion();
+        assertEquals(Regions.EUROPE, region);
+
+        model.previousRegion();
+        region = model.getSelectedRegion();
+        assertEquals(Regions.ASIA, region);
+
+        model.previousRegion();
+        region = model.getSelectedRegion();
+        assertEquals(Regions.AMERICA, region);
+
+        model.previousRegion();
+        region = model.getSelectedRegion();
+        assertEquals(Regions.EUROPE, region);
+    }
+
+    @Test
+    public void testNameCharIncrement() {
+        Application model = new Application();
+
+        for (int i = 0; i < Application.NAME_LENGTH; i++) {
+            int selectedChar = model.getSelectedChar();
+            assertEquals(i, selectedChar);
+            model.incrementSelectedChar();
+        }
+
+        int selectedChar = model.getSelectedChar();
+        assertEquals(0, selectedChar);
+    }
+
+    @Test
+    public void testNameCharDecrement() {
+        Application model = new Application();
+
+        for (int i = Application.NAME_LENGTH - 1; i >= 0; i--) {
+            model.decrementSelectedChar();
+            int selectedChar = model.getSelectedChar();
+            assertEquals(i, selectedChar);
+        }
+    }
+
+    @Test
+    public void testGetLetters() {
+        Application model = new Application();
+        int[] name = model.getName();
+        char[] letters = model.getNameCharacters();
+
+        for (int characterIndex : name) {
+            assertEquals(0, characterIndex);
+            assertEquals('A', letters[characterIndex]);
+        }
+    }
+
+    @Test
+    public void testSetLetters() {
+        Application model = new Application();
+        char[] letters = model.getNameCharacters();
+
+        for (int y = 0; y < 3; y++) {
+            for (int i = 0; i < 26; i++) {
+                int[] name = model.getName();
+                assertEquals(letters[i], letters[name[y]]);
+                model.incrementLetter();
+            }
+            int[] name = model.getName();
+            assertEquals(letters[0], letters[name[y]]);
+            model.incrementSelectedChar();
+        }
+    }
+
+    @Test
+    public void testInc() {
         Application model = new Application();
         int max = 3;
         int current = 2;
@@ -95,7 +192,7 @@ public class ApplicationTest extends AbstractTest {
     }
 
     @Test
-    public void testDec(){
+    public void testDec() {
         Application model = new Application();
         int max = 4;
         int current = 2;
