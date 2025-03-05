f52b2af252da4f942773e453195380f9194e5d1c
Simon <simon.kappeler@students.fhnw.ch>
2025-03-05 14:19:15 +0100

    Unittest schreiben

--- src/test/java/conserveandcook/model/RecipeTest.java
+++ src/test/java/conserveandcook/model/RecipeTest.java
@@ -1,8 +1,9 @@
 package conserveandcook.model;
 
 import conserveandcook.AbstractTest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,5 +40,65 @@ public class RecipeTest extends AbstractTest {
         } catch (Exception e) {
             fail(e);
         }
+    }
+
+    @Test
+    public void scoreTest() throws SQLException {
+        // Arrange
+        Recipe r1;
+        Application m = new Application();
+
+        // Act
+        r1 = Recipe.getRandomRecipe();
+        Ingredient i1 = Ingredient.getIngredientById("007");
+        Ingredient i2 = Ingredient.getIngredientById("001");
+        Ingredient i3 = Ingredient.getIngredientById("006");
+        m.addSelectedIngredient(i1);
+        m.addSelectedIngredient(i2);
+        m.addSelectedIngredient(i3);
+
+        // Assert
+        int score = r1.calculateScore(m.getSelectedIngredients());
+        assertEquals(100, score);
+    }
+
+    @Test
+    public void scoreTestMin() throws SQLException {
+        // Arrange
+        Recipe r1;
+        Application m = new Application();
+
+        // Act
+        r1 = Recipe.getRandomRecipe();
+        Ingredient i1 = Ingredient.getIngredientById("004");
+        Ingredient i2 = Ingredient.getIngredientById("009");
+        Ingredient i3 = Ingredient.getIngredientById("006");
+        m.addSelectedIngredient(i1);
+        m.addSelectedIngredient(i2);
+        m.addSelectedIngredient(i3);
+
+        // Assert
+        int score = r1.calculateScore(m.getSelectedIngredients());
+        assertEquals(300, score);
+    }
+
+    @Test
+    public void scoreTestMax() throws SQLException {
+        // Arrange
+        Recipe r1;
+        Application m = new Application();
+
+        // Act
+        r1 = Recipe.getRandomRecipe();
+        Ingredient i1 = Ingredient.getIngredientById("005");
+        Ingredient i2 = Ingredient.getIngredientById("003");
+        Ingredient i3 = Ingredient.getIngredientById("002");
+        m.addSelectedIngredient(i1);
+        m.addSelectedIngredient(i2);
+        m.addSelectedIngredient(i3);
+
+        // Assert
+        int score = r1.calculateScore(m.getSelectedIngredients());
+        assertEquals(0, score);
     }
 }
