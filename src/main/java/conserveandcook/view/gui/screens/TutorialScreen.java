9c45713831a9a0fc5bce538515ba3869d71cafdb
Simon <simon.kappeler@students.fhnw.ch>
2025-03-12 11:55:50 +0100

    Worttrennung implementiert

--- src/main/java/conserveandcook/view/gui/screens/TutorialScreen.java
+++ src/main/java/conserveandcook/view/gui/screens/TutorialScreen.java
@@ -16,14 +16,22 @@ public class TutorialScreen extends AbstractScreen {
     @Override
     public void draw(Application model) {
         drawBackground("img/tutorial/frame_" + frameIndex + ".png");
-        drawText("Willkommen bei Conserve&Cook!");
+        // TODO: (VW) "and" mit "&" ersetzen in der Font
+        drawText("01234567 8901 23456789012345678901 2345 67890123 45678901234567890");
     }
 
     private void drawText(String text) {
         gui.setTextAlignCenter();
-        Result coordinates = getCoorinates();
+        Result coordinates = getCoordinates();
         gui.setFontSize((int) (coordinates.fontsize() * SCALE));
-        gui.drawString(text, coordinates.x() * SCALE, coordinates.y() * SCALE);
+
+        String[] lines = splitStringByWordLength(text, coordinates.wordLength());
+
+        int yOffset = 0;
+        for (String line : lines) {
+            gui.drawString(line, coordinates.x() * SCALE, (coordinates.y() + yOffset) * SCALE);
+            yOffset += gui.getFontSize() + 20;
+        }
     }
 
     @Override
@@ -43,40 +51,67 @@ public class TutorialScreen extends AbstractScreen {
     }
 
 
-    private record Result(int x, int y, int fontsize) {
+    private record Result(int x, int y, int fontsize, int wordLength) {
     }
 
-    private Result getCoorinates() {
-        int x, y;
+    public String[] splitStringByWordLength(String text, int wordLength) {
+        int textLength = text.length();
+
+        if (textLength <= wordLength) {
+            return new String[]{text};
+        }
+        int requiredLines;
+        if (textLength % wordLength == 0) {
+            requiredLines = textLength / wordLength;
+        } else {
+            requiredLines = (textLength / wordLength) + 1;
+        }
+        String[] lines = new String[requiredLines];
+        for (int i = 0; i < requiredLines; i++) {
+            String line = text.substring(i, i + wordLength);
+            if (line.contains(" ")) {
+                line = line.substring(0, line.lastIndexOf(" "));
+            }
+            lines[i] = line;
+        }
+
+        return lines;
+    }
+
+    private Result getCoordinates() {
+        int x, y, wordLength = 30;
         int fontsize = 65;
 
         switch (frameIndex) {
             case 0:
-                x = 645;
-                y = 345;
+                x = 650;
+                y = 330;
                 break;
 
             case 1:
-                x = 665;
+                x = 655;
                 y = 330;
+                wordLength = 32;
                 break;
 
             case 2:
                 x = 1170;
                 y = 660;
-                fontsize = 40;
+                fontsize = 45;
                 break;
 
             case 3:
                 x = 1025;
                 y = 260;
-                fontsize = 40;
+                fontsize = 45;
+                wordLength = 35;
                 break;
 
             case 4:
                 x = 975;
                 y = 245;
                 fontsize = 50;
+                wordLength = 35;
                 break;
 
             case 5:
@@ -90,6 +125,6 @@ public class TutorialScreen extends AbstractScreen {
                 y = 100;
                 break;
         }
-        return new Result(x, y, fontsize);
+        return new Result(x, y, fontsize, wordLength);
     }
 }
