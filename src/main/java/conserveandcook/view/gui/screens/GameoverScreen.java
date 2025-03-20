31f5f03d03e8b673c079c3c1023a292ab094efff
Simon <simon.kappeler@students.fhnw.ch>
2025-03-20 10:35:09 +0100

    Automatisches weiter navigieren entfernt

--- src/main/java/conserveandcook/view/gui/screens/GameoverScreen.java
+++ src/main/java/conserveandcook/view/gui/screens/GameoverScreen.java
@@ -7,8 +7,6 @@ import conserveandcook.view.gui.AvailableScreens;
 
 public class GameoverScreen extends AbstractScreen {
 
-    private long time;
-    private final long IDLE_TIME = 5_000;
     public GameoverScreen(Gui g) {
         super(g);
     }
@@ -16,20 +14,10 @@ public class GameoverScreen extends AbstractScreen {
     public void draw(Application model) {
         String lang = model.getSelectedLanguage().toString().toLowerCase();
         drawBackground("img/gameover/" + lang + ".png");
-
-        long currentTime = System.currentTimeMillis();
-        if(currentTime - time >= IDLE_TIME) {
-            model.incrementScreen();
-        }
     }
 
     @Override
     public AvailableScreens next() {
         return AvailableScreens.RESULT;
-    }
-
-    @Override
-    public void init(Application model) {
-        time = System.currentTimeMillis();
     }
 }
