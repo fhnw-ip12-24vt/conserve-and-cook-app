80c92fca64bed53e286de69083ee772467258ded
Simon <simon.kappeler@students.fhnw.ch>
2025-03-20 11:17:19 +0100

    Button Klasse mit Interface

---
+++ src/main/java/conserveandcook/view/gui/components/Button.java
@@ -0,0 +1,15 @@
+package conserveandcook.view.gui.components;
+
+import ch.trick17.gui.Gui;
+
+public class Button implements Component {
+    @Override
+    public void draw(Gui gui) {
+
+    }
+
+    @Override
+    public void onPress(Gui gui) {
+
+    }
+}
