88b24880c3e2301f01a1208bde4a8b914b8a7fe4
Simon <simon.kappeler@students.fhnw.ch>
2025-04-01 19:18:55 +0200

    Bilder in neuen Ordner bewegt

--- src/main/java/conserveandcook/view/gui/screens/GameScreen.java
+++ src/main/java/conserveandcook/view/gui/screens/GameScreen.java
@@ -10,8 +10,6 @@ import conserveandcook.view.gui.AvailableScreens;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.Path;
-
 public class GameScreen extends AbstractScreen {
     private static final Logger log = LoggerFactory.getLogger(GameScreen.class);
 
@@ -103,7 +101,7 @@ public class GameScreen extends AbstractScreen {
         int yOffset = category * 240;
         int xOffset = ingredientIndex * 230;
 
-        gui.drawImage(path, (1090 + xOffset) * SCALE, (200 + yOffset) * SCALE, SCALE *ingredientScale);
+        gui.drawImage(path, (1090 + xOffset) * SCALE, (200 + yOffset) * SCALE, SCALE * ingredientScale);
 
         if (category == prevCategory) {
             ingredientIndex = ingredientIndex + 1 > 3 ? 0 : ingredientIndex + 1;
@@ -123,19 +121,13 @@ public class GameScreen extends AbstractScreen {
     }
 
     private static String getPathOfImage(Ingredient ingredient, Recipe selectedRecipe) {
-        String name = ingredient.getName() + ".png";
-//        name = name.substring(0, 1).toUpperCase() + name.substring(1);
-        String recipeId = String.valueOf(selectedRecipe.getId());
-
-        String path = "img/recipe/" + recipeId + "/" + name;
+        String path = "img/ingredient/" + ingredient.getName() + ".png";
         return path;
     }
 
     private void drawRecipe(Recipe recipe) {
         int x = 120, y = 235;
-        String id = String.valueOf(recipe.getId());
-        String path = Path.of("img/recipe", id, "recipe.png").toString();
-        path = path.replace('\\', '/');
+        String path = "img/recipe/" + recipe.getName() + ".png";
 
         gui.drawImage(path, x * SCALE, y * SCALE, SCALE);
     }
