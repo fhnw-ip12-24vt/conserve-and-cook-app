9a4a12e332a07a9c0b8cff1495ca6a7f95dc5d01
Simon <simon.kappeler@students.fhnw.ch>
2025-04-30 11:11:33 +0200

    Buttons zu LanguageScreen hinzugefügt

--- src/main/java/conserveandcook/view/gui/screens/LanguageScreen.java
+++ src/main/java/conserveandcook/view/gui/screens/LanguageScreen.java
@@ -4,13 +4,31 @@ import ch.trick17.gui.Gui;
 import conserveandcook.model.Application;
 import conserveandcook.view.gui.AbstractGif;
 import conserveandcook.view.gui.AvailableScreens;
+import conserveandcook.view.gui.components.Button;
+
+import java.util.List;
 
 public class LanguageScreen extends AbstractGif {
     static final int FRAMES = 4;
     static final long GIF_INTERVAL = 2200 * 100000; // 0.25 seconds
 
+    private final Button[] buttons;
+
     public LanguageScreen(Gui gui) {
         super(gui, FRAMES, GIF_INTERVAL);
+
+        buttons = new Button[4];
+        for (int i = 0; i < 4; i++) {
+            buttons[i] = new Button(Button.Type.NO_GUI);
+        }
+        components.addMany(List.of(buttons));
+    }
+
+    @Override
+    public void init(Application model) {
+        for (int i = 0; i < 4; i++) {
+            buttons[i].setOnPress(model::incrementScreen);
+        }
     }
 
     @Override
