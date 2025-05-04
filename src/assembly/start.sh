af30e3f7c80514f1fbe830b94022d4a851f0102d
Athavan Indrakumaran <athavani3c@gmail.com>
2025-05-04 23:21:14 +0200

    autostart implementiert

--- src/assembly/start.sh
+++ src/assembly/start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-cd "$1"
-pkill java
-DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -jar "$2".jar
-exit 0
+
+DEPLOY_DIR="/home/pi/deploy/Trick17App/"
+
+JAR_NAME="Trick17App"
+SERVICE_NAME="trick17app"
+
+SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
+TEMPLATE_FILE="${DEPLOY_DIR}/${JAR_NAME}/autostart/trick17app.service.template"
+
+#echo "Installing Trick17 autostart service..."
+
+if [ -f "$TEMPLATE_FILE" ]; then
+    sudo cp "$TEMPLATE_FILE" "$SERVICE_FILE"
+    sudo systemctl daemon-reload
+    sudo systemctl enable "$SERVICE_NAME"
+#    echo "Service installed and enabled to start on boot."
+#else
+#    echo "ERROR: Service template not found at $TEMPLATE_FILE"
+fi
+
+sudo systemctl start "$SERVICE_NAME"
