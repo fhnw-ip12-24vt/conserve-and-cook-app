7c9617cd95774cb44d59571ab0dac5887e9d6318
Simon <simon.kappeler@students.fhnw.ch>
2025-05-05 11:38:34 +0200

    Fix mehrere Prozesse

--- src/assembly/start.sh
+++ src/assembly/start.sh
@@ -8,11 +8,10 @@ SERVICE_NAME="trick17app"
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
 TEMPLATE_FILE="${DEPLOY_DIR}/autostart.service"
 
-#echo "Installing Trick17 autostart service..."
-
 if [ -f "$TEMPLATE_FILE" ]; then
     sudo cp "$TEMPLATE_FILE" "$SERVICE_FILE"
     sudo systemctl daemon-reload
+    sudo systemctl stop "$SERVICE_NAME"
     sudo systemctl enable "$SERVICE_NAME"
     sudo systemctl start "$SERVICE_NAME"
     echo "Service installed and enabled to start on boot."
@@ -21,7 +20,4 @@ else
     exit 1
 fi
 
-cd "$1"
-pkill java
-DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -jar "$2".jar
 exit 0
