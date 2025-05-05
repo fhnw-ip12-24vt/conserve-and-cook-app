13a40dde65f26843f9a26544925c73a1d99d87e8
Simon <simon.kappeler@students.fhnw.ch>
2025-05-05 11:20:04 +0200

    Autostart in Raspi integriert

--- src/assembly/start.sh
+++ src/assembly/start.sh
@@ -6,7 +6,7 @@ JAR_NAME="Trick17App"
 SERVICE_NAME="trick17app"
 
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
-TEMPLATE_FILE="${DEPLOY_DIR}/${JAR_NAME}/autostart/trick17app.service.template"
+TEMPLATE_FILE="${DEPLOY_DIR}/autostart.service"
 
 #echo "Installing Trick17 autostart service..."
 
@@ -14,9 +14,14 @@ if [ -f "$TEMPLATE_FILE" ]; then
     sudo cp "$TEMPLATE_FILE" "$SERVICE_FILE"
     sudo systemctl daemon-reload
     sudo systemctl enable "$SERVICE_NAME"
-#    echo "Service installed and enabled to start on boot."
-#else
-#    echo "ERROR: Service template not found at $TEMPLATE_FILE"
+    sudo systemctl start "$SERVICE_NAME"
+    echo "Service installed and enabled to start on boot."
+else
+    echo "ERROR: Service template not found at $TEMPLATE_FILE"
+    exit 1
 fi
 
-sudo systemctl start "$SERVICE_NAME"
+cd "$1"
+pkill java
+DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -jar "$2".jar
+exit 0
