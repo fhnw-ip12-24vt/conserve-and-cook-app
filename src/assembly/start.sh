16188bd885e041d02b9d692fa11467bf8971a497
Ahmed Ali Gaber <ahmed.aligaber@students.fhnw.ch>
2025-05-06 14:27:42 +0200

    Done

--- src/assembly/start.sh
+++ src/assembly/start.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+# Check if Java is installed
+if type -p java; then
+    echo "Java found in PATH."
+elif [[ -x "$JAVA_HOME/bin/java" ]]; then
+    echo "Java found in JAVA_HOME."
+else
+    echo "Java not found. Installing JDK 21..."
+
+    # For Debian/Ubuntu systems (requires sudo privileges)
+    sudo apt update
+    sudo apt install -y openjdk-21-jdk
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to install JDK 21. Exiting."
+        exit 1
+    fi
+fi
+
 cd "$1"
 pkill java
 DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -jar "$2".jar
