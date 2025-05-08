1da52bbd3c1e1f4e529ad6584cf94af2c62ece13
Simon <simon.kappeler@students.fhnw.ch>
2025-05-08 14:46:38 +0200

    Merge Konflikt gelöst

---
+++ src/assembly/autostart.sh
@@ -0,0 +1,18 @@
+# Check if Java is installed
+if type java; then
+    echo "Java found in PATH."
+elif [[ -x "usr/bin/java" ]]; then
+    echo "Java found in JAVA_HOME."
+else
+    echo "Java not found. Installing JDK 21..."
+
+    # For Debian/Ubuntu systems (requires sudo privileges)
+    sudo apt update
+    sudo apt install -y openjdk-21-jdk
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to install JDK 21. Exiting."
+        exit 1
+    fi
+fi
+/usr/bin/java -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx2G -jar /home/pi/deploy/Trick17App/Trick17App.jar
